f25a642374c2d09d281fe47e70e8f134bd3d98ac
Ash <aakash10ash@gmail.com>
2023-08-07 19:02:27 +0530

    Task from DSS enhancement done

--- src/App.tsx
+++ src/App.tsx
@@ -70,7 +70,29 @@ function App() {
 
   const handleSubmit = (e: any) => {
     e.preventDefault();
-    setTotal([...total, values]);
+    if (!values.username.match("^[a-z]{0,15}$"))
+      alert("Username is not in valid format");
+    else if (!values.address.match("^[a-z0-9A-Z]{8,}$"))
+      alert("Address is not in valid format");
+    else if (!values.country.match("^[A-Za-z]{3,}$"))
+      alert("Country is not in valid format");
+    else if (!values.email.match("[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,}$"))
+      alert("Email is not in valid format");
+    else if (!values.password.match("(?=.*d)(?=.*[a-z])(?=.*[A-Z]).{8,}"))
+      alert("Password is not in valid format");
+    else if (!values.phone.match("^[0-9]{10}$"))
+      alert("Phone number is not in valid format");
+    else {
+      setTotal([...total, values]);
+      setValues({
+        username: "",
+        email: "",
+        phone: "",
+        country: "",
+        address: "",
+        password: "",
+      });
+    }
   };
 
   const onChange = (e: any) => {
@@ -86,77 +108,83 @@ function App() {
   //console.log(globalThis.XXX);
 
   const viewData = () => {
-    console.log(values.password);
-    setResult(true);
+    //   console.log(values.password);
+    setResult(!result);
   };
 
   return (
     <>
-      <form onSubmit={handleSubmit}>
-        <Form
-          name="username"
-          type="text"
-          placeholder="Enter your name"
-          label="Username"
-          errormessage="Username should be 3-16 characters and shouldn't include any special character!"
-          onChange={onChange}
-          pattern="^[A-Za-z0-9]{3,16}$"
-          required={true}
-        />
-        <Form
-          name="password"
-          type="password"
-          placeholder="Create your new password"
-          label="Password"
-          errormessage="Must contain at least one  number and one uppercase and lowercase letter, and at least 8 or more characters"
-          onChange={onChange}
-          required={true}
-          pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
-        />
-        <Form
-          name="email"
-          type="text"
-          placeholder="Enter your email"
-          label="email"
-          errormessage="Enter your valid email id"
-          onChange={onChange}
-          required={true}
-          pattern="[a-z0-9._%+\-]+@[a-z0-9.\-]+\.[a-z]{2,}$"
-        />
-        <Form
-          name="phone"
-          type="tel"
-          placeholder="Enter your phone number"
-          label="phone"
-          errormessage="Enter 10 digit number"
-          onChange={onChange}
-          pattern="[0-9]{10,}$"
-          required={true}
-        />
-        <Form
-          name="address"
-          type="text"
-          placeholder="Enter your address"
-          label="address"
-          errormessage="Enter your address"
-          onChange={onChange}
-          required={true}
-          pattern="^[A-Za-z0-9]{8,16}$"
-        />
-        <Form
-          name="country"
-          type="text"
-          placeholder="Enter your country"
-          label="country"
-          errormessage="Country should be four digits"
-          onChange={onChange}
-          required={true}
-          pattern="^[A-Za-z]{4,16}$"
-        />
-        <button>Submit</button>
-      </form>
-      <div>
-        <button onClick={viewData}> View Data </button>
+      <div className="app">
+        <form onSubmit={handleSubmit}>
+          <Form
+            name="username"
+            type="text"
+            placeholder="Enter your name"
+            label="Username"
+            errormessage="Username should be 3-16 characters and shouldn't include any special character!"
+            onChange={onChange}
+            value={values.username}
+            pattern="^[A-Za-z0-9]{3,16}$"
+            required={false}
+          />
+          <Form
+            name="password"
+            type="password"
+            placeholder="Create your new password"
+            label="Password"
+            errormessage="Must contain at least one  number and one uppercase and lowercase letter, and at least 8 or more characters"
+            onChange={onChange}
+            value={values.password}
+            required={false}
+            pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
+          />
+          <Form
+            name="email"
+            type="text"
+            placeholder="Enter your email"
+            label="Email"
+            errormessage="Enter your valid email id"
+            onChange={onChange}
+            value={values.email}
+            required={false}
+            pattern="[a-z0-9._%+\-]+@[a-z0-9.\-]+\.[a-z]{2,}$"
+          />
+          <Form
+            name="phone"
+            type="tel"
+            placeholder="Enter your phone number"
+            label="Phone"
+            errormessage="Enter 10 digit number"
+            onChange={onChange}
+            value={values.phone}
+            pattern="[0-9]{10,}$"
+            required={false}
+          />
+          <Form
+            name="address"
+            type="text"
+            placeholder="Enter your address"
+            label="Address"
+            errormessage="Enter your address"
+            onChange={onChange}
+            value={values.address}
+            required={false}
+            pattern="^[A-Za-z0-9]{8,16}$"
+          />
+          <Form
+            name="country"
+            type="text"
+            placeholder="Enter your country"
+            label="Country"
+            errormessage="Country should be four values"
+            onChange={onChange}
+            value={values.country}
+            required={false}
+            pattern="^[A-Za-z]{4,16}$"
+          />
+          <button>Submit</button>
+        </form>
+        <button onClick={viewData}> {result ? "HideData" : "ViewData"} </button>
         {result ? <ResultTable res={total} /> : null}
       </div>
     </>
--- src/dss_react.tsx
+++ src/dss_react.tsx
@@ -1,5 +1,8 @@
 import React, { useState } from "react";
 import "./dss.css";
+import "./App.css";
+
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function Form(props: any) {
   const [focused, setFocused] = useState(false);
@@ -11,8 +14,9 @@ function Form(props: any) {
 
   return (
     <div>
-      <label>{props.label}</label>
+      <label className="label">{props.label}</label> {"  "}
       <input
+        className="input"
         {...props}
         onChange={props.onChange}
         onBlur={handleFocus}
